32c9358e4b26a04cf54903af04577566c1154fd7
Ahad-UCF <Ahadbawany@knights.ucf.edu>
2019-06-15 13:39:32 -0400

    added ability to parse CSV, does NOT support the actual quiz yet.

--- quiz.go
+++ quiz.go
@@ -3,19 +3,34 @@ package main
 import(
 	"bufio"
 	"fmt"
-	//"encoding/csv"
+	"encoding/csv"
 	"os"
+	"io"
 )
 
 func main(){
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Printf("Enter the name of your quiz CSV, if using default quiz simply press enter: ")
-	text, _ := reader.ReadString('\n')
+	var i int = 0
+	cmdReader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Enter the name of your quiz csv, if using default quiz simply press enter: ")
+	text, _ := cmdReader.ReadString('\n')
 	if text == "\n"{
-		text = ""
+		text = "Quiz.csv"
 		fmt.Println("Using default quiz file.")
 	} else {
 		fmt.Printf("Using %s", text)
 	}
-
+	quizFile, _ := os.Open(text)
+	csvReader := csv.NewReader(bufio.NewReader(quizFile))
+	for {
+		line, error := csvReader.Read()
+		if error == io.EOF{
+			fmt.Printf("Finished!\n")
+			break
+		} else if error != nil{
+			fmt.Printf("%s", error)
+		} else {
+			i++
+			fmt.Printf("%s", line)
+		}
+	}
 }
